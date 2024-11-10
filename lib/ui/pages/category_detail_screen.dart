d32bf94fe05f2fde7f480ed13bf74c385f9e30ae
dhanazam <ramadanazam93@gmail.com>
2024-11-10 21:36:14 +0800

    add blank screen

---
+++ lib/ui/pages/category_detail_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CategoryDetailScreen extends StatelessWidget {
+  const CategoryDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CategoryDetail'),
+      ),
+      body: const Center(
+        child: Text('CategoryDetail'),
+      ),
+    );
+  }
+}
